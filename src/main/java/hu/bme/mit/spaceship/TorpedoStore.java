6a2cd5d7482bc141faf234264900eeab26bc81e9
Gabor Pelesz <gaborpelesz@hotmail.com>
2020-04-30 19:09:59 +0200

    Complete statement with missing 'throw' keyword

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -32,7 +32,7 @@ public class TorpedoStore {
 
   public boolean fire(final int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
